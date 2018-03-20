2e27f074093fd937237b6c2d9aa266ba6c0e555f
Sam <crisamdegracia@gmail.com>
2018-03-21 02:41:21 +0300

    initial commit: modifying video background

--- wordpress/wordpress/wp-content/themes/bootstrap2wordpress/footer.php
+++ wordpress/wordpress/wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,24 +11,30 @@
 
 ?>
 
-	</div><!-- #content -->
+<script type="text/javascript" src="<?php bloginfo('template_directory') ?>/js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script type="text/javascript" src="<?php bloginfo('template_directory')?>/bootstrap/js/bootstrap.min.js"></script>
+<script src="/wordpress/wordpress/wp-content/plugins/video-background/js/vidbg.js"></script>
+<script src="/wordpress/wordpress/wp-content/plugins/video-background/js/vidbg-backend-min.js"></script>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' );
+<?php wp_footer();  ?>
+
+
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' );
 			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="https://crisamdegracia.github.io">Sam</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+		<span class="sep"> | </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="https://crisamdegracia.github.io">Sam</a>' );
+		?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 
-<?php wp_footer(); ?>
+
 
 </body>
 </html>
---
+++ wordpress/wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
@@ -0,0 +1,38 @@
+<?php /* Template Name: Home Page */
+
+
+
+get_header();
+
+
+?>
+
+
+
+
+
+
+<!-- CAROUSEL SECTION -->		
+
+<!--
+-->
+
+
+
+<!--VIDEO BACKGROUND -->
+
+<div class="container-fluid"  id="x">
+	<div class="video-header">
+		<h1> Welcome To Sushi Boutique</h1>
+	</div>
+</div>
+
+<div class="container py-5">
+	<div  id="primary" >
+
+
+	</div><!--primary-->
+</div><!--container-->
+
+
+<?php get_footer() ?>
