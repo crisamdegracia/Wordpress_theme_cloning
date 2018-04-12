0fbe4a40766e9a9b250187ee92aa0141dd4474d8
Sam <crisamdegracia@gmail.com>
2018-04-12 03:41:16 +0300

    menu category : toggling works

--- wp-content/themes/bootstrap2wordpress/footer.php
+++ wp-content/themes/bootstrap2wordpress/footer.php
@@ -99,30 +99,41 @@
 
 <script type="text/javascript">
 
-	var menu_category_h1 		= $('#menu_section_container h1'),
+	var menu_category_h1		= $('#menu_section_container h1'),
 			menu_category_body 	= $('.menu-category-body'),
 			menu_category_text = $('.menu-category-text'),
 			menu_category_content = $('.menu-category-content')
-
+			
+			
 	menu_category_content.append('<div class="menu-pop-btn"><i class="menu-fas fa fa-eye"></i></div>');
 	menu_category_viewed = $('.menu-fas');
 	menu_pop_btn = $('.menu-pop-btn');
-
+	
+	menu_category_body.prepend('<i class="toggle-categories fa fa-arrow-circle-up"></i>');
+	toggle_categories			= $('.toggle-categories')
+	
+	
 	menu_category_body.hide();
 	menu_category_text.hide();
 	
 	menu_category_h1.click(function(){
-		$(this).next().fadeToggle();
+			$(this).next().slideToggle();
 	})
-
+	
+ 	toggle_categories.click(function(){
+		
+		$(this).parent().fadeToggle();
+		
+	})
+ 
 
 	menu_pop_btn.click(function(){
 		
 		/* looking for the content of the items of the menu */
 		$(this).parent().find(menu_category_text).slideToggle();
 		
+		/* toggles the eye icons */
 		$(this).find('i').toggleClass('fa-eye-slash')
-		//		$(this).find('i').removeClass('fa-eye-slash').addClass('fa-eye')
 	})
 
 
